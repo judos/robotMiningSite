23309abce6b88947f4444850713d7eff1543ab53
judos <judos@gmx.ch>
2016-04-19 01:14:18 +0200

    added technology, missing migration script

--- source/prototypes/technology.lua
+++ source/prototypes/technology.lua
@@ -1,2 +1,21 @@
-addTechnologyUnlocksRecipe("construction-robotics","robotMiningSite")
+data:extend({
+  {
+    type = "technology",
+    name = "mining-robotics",
+    icon = "__base__/graphics/technology/construction-robotics.png",
+    prerequisites = {"construction-robotics"},
+    unit = {
+      count = 100,
+      ingredients =
+      {
+        {"science-pack-1", 3},
+        {"science-pack-2", 1}
+      },
+      time = 20
+    },
+    order = "_mining",
+  }
+})
 
+addTechnologyUnlocksRecipe("mining-robotics","robotMiningSite")
+addTechnologyUnlocksRecipe("mining-robotics","mining-robot")
