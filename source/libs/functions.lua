f453b108d335b122b7987feb2094d56963c7fc57
judos <judos@gmx.ch>
2016-04-19 01:16:46 +0200

    fixed issue when using tech function

--- source/libs/functions.lua
+++ source/libs/functions.lua
@@ -3,9 +3,12 @@ require "libs.basic-lua-extensions"
 
 -- adds a recipe which is unlocked when the given technology is researched
 function addTechnologyUnlocksRecipe(technologyName, recipeName)
-	if data.raw["technology"][technologyName] then
-		table.insert(data.raw["technology"][technologyName].effects,
-			{ type = "unlock-recipe", recipe = recipeName })
+	local tech = data.raw["technology"][technologyName]
+	if tech then
+		if not tech.effects then
+			tech.effects = {}
+		end
+		table.insert(tech.effects, {type = "unlock-recipe", recipe = recipeName })
 	else
 		error("Technology "..technologyName.." not found. Did you mean?")
 		for name,_ in pairs(data.raw["technology"]) do
