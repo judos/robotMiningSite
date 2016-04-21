8a605fdd020d5e1845220c63f65ea5e915657539
judos <judos@gmx.ch>
2016-04-21 21:39:17 +0200

    omg my turrets please don't shoot mining robots :'D

--- source/control/forces.lua
+++ source/control/forces.lua
@@ -3,12 +3,13 @@ forceSuffix = "-miningRobots"
 -- Fetch the special mining force for an entity
 function miningForceFor(entity)
 	local forceName = entity.force.name
-	if not type(forceName) == "string" then
-		forceName = entity.force.name
-	end
+
 	if not forceName:ends(forceSuffix) then
 		forceName = forceName..forceSuffix
-		if not game.forces[forceName] then game.create_force(forceName) end
+		if not game.forces[forceName] then
+			game.create_force(forceName) 
+			entity.force.set_cease_fire(forceName,true)
+		end
 	end
 	
 	return forceName
