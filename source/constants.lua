4770bc92999650582c3f082b82f07147fcb62d2f
judos <judos@gmx.ch>
2016-10-17 15:45:10 +0200

    fix logging

--- source/constants.lua
+++ source/constants.lua
@@ -3,8 +3,4 @@ modVersion = "0.4.1"
 modName = "robotMiningSite"
 fullModName = "robotMiningSite"
 
-local testing = true
-if testing then
-	debug_master = true -- Master switch for debugging, prints debug stuff into the shell where factorio was started from
-	debug_level = 1 -- 1=info 2=warning 3=error
-end
+libLog.testing = true
