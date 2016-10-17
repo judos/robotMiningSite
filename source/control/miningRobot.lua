6bd68804547410a9f98f3e74f001a618181c64d7
judos <judos@gmx.ch>
2016-10-17 15:18:17 +0200

    improved placing of robots

--- source/control/miningRobot.lua
+++ source/control/miningRobot.lua
@@ -2,7 +2,6 @@
 entities["mining-robot"] = {
 	build = function(entity)
 		local forceName = miningForceForEntity(entity)
-		local newRobot = entity.surface.create_entity({name=entity.name,position=entity.position,force = forceName})
-		entity.destroy()
+		entity.force = forceName
 	end
 }
