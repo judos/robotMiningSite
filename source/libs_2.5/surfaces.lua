1d5d00f2a18562892fb6e96b289de1ac72ab3454
judos <judos@gmx.ch>
2017-07-17 20:55:29 +0200

    remove old lib folder

--- source/libs_2.5/surfaces.lua
+++
@@ -1,9 +0,0 @@
-function surfaceWithIndex(index)
-	for name,surface in pairs(game.surfaces) do
-		if surface.index == index then
-			return surface
-		end
-	end
-	warn("Surface with index "..tostring(index).." was not found")
-	return nil
-end
