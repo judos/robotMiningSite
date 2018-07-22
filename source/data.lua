e4895a788381cf278da3d64b9bca6cf4c7cc6e50
Cordina90 <pierbraaksma@hotmail.com>
2018-07-22 19:39:00 +0200

    added additional check for optional mods
    When none of the optional dependencies is available, the settings are not activated, so they cannot be read for their value...

--- source/data.lua
+++ source/data.lua
@@ -5,14 +5,17 @@ if not robotMiningSite.settings then robotMiningSite.settings = {} end
 if settings.startup["robotMiningSite_EasyT1"].value == true then
   robotMiningSite.settings.easyT1 = true
 end
-if settings.startup["useBobRobotics"].value == true  then
-	robotMiningSite.settings.bobs = true
-end
-if settings.startup["useBotGrades"].value == true  then
-	robotMiningSite.settings.botsmk = true
-end
-if settings.startup["needPreviousGrade"].value == true  then
-	robotMiningSite.settings.botsUpgrade = true
+
+if mods["boblogistics"] then
+  if settings.startup["useBobRobotics"].value == true  then
+    robotMiningSite.settings.bobs = true
+  end
+  if settings.startup["useBotGrades"].value == true  then
+    robotMiningSite.settings.botsmk = true
+  end
+  if settings.startup["needPreviousGrade"].value == true  then
+    robotMiningSite.settings.botsUpgrade = true
+  end
 end
 
 require "config"
