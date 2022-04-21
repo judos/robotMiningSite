a802fb270791754e7f10ce747653748eda5375a6
Cordina90 <pierbraaksma@hotmail.com>
2022-04-21 15:28:54 +0200

    v0.11.0
    Updated for Factorio 1.1 with updated recipes when using Bobs Logistics

--- source/prototypes/miningRobot-recipe-updates.lua
+++ source/prototypes/miningRobot-recipe-updates.lua
@@ -91,9 +91,7 @@ if robotMiningSite.settings.botsmk then
 	  add_ingredient("mining-robot-2", {"advanced-circuit", 1})
 	end
 
-	if data.raw["mining-tool"]["brass-axe"] then
-	  add_ingredient("mining-robot-2", {"brass-axe", 2})
-	elseif data.raw["item"]["brass-alloy"] then
+	if data.raw["item"]["brass-alloy"] then
 	  add_ingredient("mining-robot-2", {"brass-alloy", 2})
 	else
 	  add_ingredient("mining-robot-2", {"steel-plate", 3})
@@ -105,10 +103,8 @@ if robotMiningSite.settings.botsmk then
 	  add_ingredient("mining-robot-3", {"processing-unit", 1})
 	end
 
-	if data.raw["mining-tool"]["brass-axe"] then
-	  add_ingredient("mining-robot-3", {"cobalt-axe", 2})
-	elseif data.raw["item"]["cobalt-plate"] then
-	  add_ingredient("mining-robot-3", {"cobalt-plate", 2})
+	if data.raw["item"]["titanium-plate"] then
+	  add_ingredient("mining-robot-3", {"titanium-plate", 2})
 	else
 	  add_ingredient("mining-robot-3", {"steel-plate", 4})
 	end
@@ -124,15 +120,23 @@ if robotMiningSite.settings.botsmk then
 	  end
 	end
 
-	if data.raw["mining-tool"]["titanium-axe"] then
-	  add_ingredient("mining-robot-4", {"titanium-axe", 2})
-	elseif data.raw["item"]["titanium-plate"] then
-	  add_ingredient("mining-robot-4", {"titanium-plate", 2})
+	if data.raw["item"]["nitinol-plate"] then
+	  add_ingredient("mining-robot-4", {"nitinol-plate", 2})
 	else
 	  add_ingredient("mining-robot-4", {"steel-plate", 5})
 	end
 
-	add_ingredient("mining-robot-5", {"fusion-reactor-equipment", 1})
+  if data.raw.item["rtg"] and data.raw.technology["rtg"] then
+    bobmods.lib.recipe.add_ingredient("mining-robot-5", {"rtg", 1})
+    bobmods.lib.tech.add_prerequisite("mining-robot-5", "rtg")
+  elseif data.raw.item["vehicle-fusion-cell-1"] and data.raw.technology["vehicle-fusion-cell-equipment-1"] then
+    bobmods.lib.recipe.add_ingredient("mining-robot-5", {"vehicle-fusion-cell-1", 1})
+    bobmods.lib.tech.add_prerequisite("mining-robot-5", "vehicle-fusion-cell-equipment-1")
+  else
+    bobmods.lib.recipe.add_ingredient("mining-robot-5", {"fusion-reactor-equipment", 1})
+    bobmods.lib.tech.add_prerequisite("mining-robot-5", "fusion-reactor-equipment")
+  end
+
 
 	if robotMiningSite.settings.botsUpgrade == true then
 	  add_ingredient("mining-robot-5", {"mining-robot-4", 1})
@@ -166,12 +170,9 @@ if robotMiningSite.settings.botsmk then
 	  end
 	end
 
-	  if data.raw["mining-tool"]["diamond-axe"] then
-		add_ingredient("mining-robot-5", {"diamond-axe", 2})
-	  elseif data.raw["item"]["gold-plate"] then
-		add_ingredient("mining-robot-5", {"gold-plate", 2})
-		if data.raw["item"]["diamond-5"] then add_ingredient("mining-robot-5", {"diamond-5", 2}) end
-	  else
-		add_ingredient("mining-robot-5", {"copper-plate", 10})
-	  end
+	if data.raw["item"]["diamond-5"] then
+	  add_ingredient("mining-robot-5", {"diamond-5", 2})
+	else
+	  add_ingredient("mining-robot-5", {"steel-plate", 10})
+	end
 end
